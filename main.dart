ef3f422f0f9784f54401f53e34a33b166038a056
Anos <bebem4@bebes-MacBook-Air.local>
2026-08-24 20:58:28 +0300

    /

--- main.dart
+++ main.dart
@@ -1,50 +1,45 @@
 void main (){
+ 
+List<Map<String,dynamic>> cart = [
+    {"name": "Apple", "price": 1000, "quantity": 3},
+  {"name": "Milk", "price": 20, "quantity": 2},
+];
 
-Product p1 = Product("VV", 500, 1);
-
-print(p1.calculateTotal());
-
-p1.printReceipt(50);
-
-
+print(TotalPrice(cart));
 }
 
-class Product{
-  String name;
-  double price;
-  int quantity;
 
-  Product(this.name , this.price , this.quantity);
+double TotalPrice (List<Map<String,dynamic>> cart){
 
-  double calculateTotal(){
-    return price * quantity;
-  }
+double subtotal = 0;
+double discount = 0;
 
-  double applyDiscount(double percentage){
-    double discount = calculateTotal() * percentage /100;
-    return calculateTotal() - discount ;
-  }
+for (var element in cart) {
 
-  void printReceipt(double percentageDiscount){
-print("your name is : $name");
-print("then price is : $price");
-
-print("the quantity is : $quantity");
-
-print( applyDiscount(percentageDiscount));
+  subtotal += element['price'] * element['quantity'] ;
   
-  }
+}
+
+if (subtotal >= 100) {
+  discount = subtotal * 0.10;
+}
+
+return subtotal - discount;
 
 }
 
-// 4. Product Discount System
-
-// Create a Product class with:
-// Attributes: name, price, quantity
-// A constructor to initialize them.
 
 
-// A method calculateTotal() that returns:
-// price × quantity
-// A method applyDiscount(double percentage) that returns the price after applying the discount.
-// A method printReceipt() that displays the product name, quantity, total price, and discounted price.
+
+// 3.Smart Shopping Cart
+
+
+// You have a shopping cart:
+// [
+//   {"name": "Apple", "price": 10, "quantity": 3},
+//   {"name": "Milk", "price": 20, "quantity": 2},
+//   {"name": "Bread", "price": 15, "quantity": 1},
+// ]
+// Calculate the total price.
+// But there's a rule:
+// If the total is greater than or equal to 100, give the customer a 10% discount.
