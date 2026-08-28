396839dc3f19f3769013f329c71705a658ec8cf6
Anos <bebem4@bebes-MacBook-Air.local>
2026-08-28 10:05:52 +0300

    //

--- main.dart
+++ main.dart
@@ -1,45 +1,64 @@
-void main (){
+void main() {
+
+String name = "hello";
+
+ List<String>  stack = [];
+
+ for (var i = 0; i < name.length; i++) {
+
+  final ch = name[i];
+
+  stack.add(ch);
+
+   
+ }
+   stack.add('R');
+   print(stack);
+   stack.removeLast();
+   print(stack);
+
+   
+    
+  }
  
-List<Map<String,dynamic>> cart = [
-    {"name": "Apple", "price": 1000, "quantity": 3},
-  {"name": "Milk", "price": 20, "quantity": 2},
-];
 
-print(TotalPrice(cart));
+
+
+class Solution {
+  bool isValid(String s) {
+    List<String>  stack = [];
+
+    for (var i = 0; i < s.length; i++) {
+      final ch = s[i];
+
+      if (ch == "(" || ch == "[" || ch == "{") {
+        stack.add(ch);
+        
+      }else{
+        if (stack.isEmpty) {
+          return false;
+          
+        }
+
+        final top = stack.removeLast();
+        if (ch == ")" && top != "(") {
+return false;
+          
+          
+        }
+        if (ch == "]" && top != "[") {
+return false;
+          
+          
+        }
+        if (ch == "}" && top != "{") {
+return false;
+          
+          
+        }
+      }
+      
+    }
+    return stack.isEmpty;
+  }
 }
-
-
-double TotalPrice (List<Map<String,dynamic>> cart){
-
-double subtotal = 0;
-double discount = 0;
-
-for (var element in cart) {
-
-  subtotal += element['price'] * element['quantity'] ;
-  
-}
-
-if (subtotal >= 100) {
-  discount = subtotal * 0.10;
-}
-
-return subtotal - discount;
-
-}
-
-
-
-
-// 3.Smart Shopping Cart
-
-
-// You have a shopping cart:
-// [
-//   {"name": "Apple", "price": 10, "quantity": 3},
-//   {"name": "Milk", "price": 20, "quantity": 2},
-//   {"name": "Bread", "price": 15, "quantity": 1},
-// ]
-// Calculate the total price.
-// But there's a rule:
-// If the total is greater than or equal to 100, give the customer a 10% discount.
