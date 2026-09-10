b740181556f9a16c332cdd9faeb2127def7c9f26
Anos <bebem4@bebes-MacBook-Air.local>
2026-09-10 18:12:02 +0300

    /

--- main.dart
+++ main.dart
@@ -1,64 +1,16 @@
 void main() {
 
-String name = "hello";
 
- List<String>  stack = [];
+  print(Animal.x);
 
- for (var i = 0; i < name.length; i++) {
 
-  final ch = name[i];
 
-  stack.add(ch);
-
-   
- }
-   stack.add('R');
-   print(stack);
-   stack.removeLast();
-   print(stack);
-
-   
-    
   }
  
 
+ class Animal {
+ static int x = 4;
+ }
 
 
-class Solution {
-  bool isValid(String s) {
-    List<String>  stack = [];
 
-    for (var i = 0; i < s.length; i++) {
-      final ch = s[i];
-
-      if (ch == "(" || ch == "[" || ch == "{") {
-        stack.add(ch);
-        
-      }else{
-        if (stack.isEmpty) {
-          return false;
-          
-        }
-
-        final top = stack.removeLast();
-        if (ch == ")" && top != "(") {
-return false;
-          
-          
-        }
-        if (ch == "]" && top != "[") {
-return false;
-          
-          
-        }
-        if (ch == "}" && top != "{") {
-return false;
-          
-          
-        }
-      }
-      
-    }
-    return stack.isEmpty;
-  }
-}
--- task.cpp
+++ task.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
-#include <array>
-#include <vector>
-
 using namespace std;
 
 
-int main (){}
+int main()
+{
+
+}
